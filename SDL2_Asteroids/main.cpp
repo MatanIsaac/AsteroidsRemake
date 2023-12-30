c66ba22fddbe649f71a510140417cb719021d682
MTN95 <113786893+MTN95@users.noreply.github.com>
2023-12-30 09:02:50 +0200

    Update main.cpp

--- SDL2_Asteroids/main.cpp
+++ SDL2_Asteroids/main.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
 #include "Game.h"
 
+/*
+* TODO:
+* 
+* 1: Some SFX are too loud.
+* 2: Game Needs a Score Count.
+* 3: A Way To Randomize the game a little
+* 4: Tidy up code and add documentation to everything.
+*/ 
+
 int main(int argc, char* argv[])
 {
 	auto game = Game::Getinstance();
